b9e7082469cfde728d444e65891acfdbd880bca9
Marco Araujo <marco.araujo.junior@gmail.com>
2016-12-11 13:40:43 -0200

    Set json format

--- model/invoice.go
+++ model/invoice.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Invoice struct {
-	ID             uint      `sql:"AUTO_INCREMENT" gorm:"primary_key"`
-	ReferenceMonth int
-	ReferenceYear  int
-	Document       string    `sql:"size:14"`
-	Description    string    `sql:"size:255"`
-	Amount         float32   `sql:"type:decimal(10,2)"`
-	IsActive       bool      `sql:"default:1"`
-	CreatedAt      time.Time `sql:"type:datetime;default:current_timestamp"`
-	DeactiveAt     time.Time `sql:"type:datetime"`
+	ID             uint      `json:"-";sql:"AUTO_INCREMENT" gorm:"primary_key"`
+	ReferenceMonth int       `json:"reference_month"`
+	ReferenceYear  int       `json:"reference_year"`
+	Document       string    `json:"document";sql:"size:14"`
+	Description    string    `json:"description";sql:"size:255"`
+	Amount         float32   `json:"amount";sql:"type:decimal(10,2)"`
+	IsActive       bool      `json:"is_active";sql:"default:1"`
+	CreatedAt      time.Time `json:"created_at";sql:"type:datetime;default:current_timestamp"`
+	DeactiveAt     time.Time `json:"deactive_at,omitempty";sql:"type:datetime"`
 }
 
 func GetAll() *gorm.DB {
