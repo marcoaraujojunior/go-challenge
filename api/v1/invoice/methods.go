facd856fd71bcad4e7a22a4a67851b3e42c14746
Marco Araujo <marco.araujo.junior@gmail.com>
2016-12-14 21:26:15 -0200

    Add Create endpoint

--- api/v1/invoice/methods.go
+++ api/v1/invoice/methods.go
@@ -2,7 +2,6 @@ package invoice
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"model"
 	"github.com/gorilla/mux"
@@ -27,26 +26,54 @@ func GetInvoice(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "CreateInvoice!")
+	var invoice model.Invoice
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := r.Body.Close(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := json.Unmarshal(body, &invoice); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	err = model.Create(&invoice)
+	if (err != nil) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func UpdateInvoice(w http.ResponseWriter, r *http.Request) {
 	var invoice model.Invoice
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	if err := r.Body.Close(); err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	if err := json.Unmarshal(body, &invoice); err != nil {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		if err := json.NewEncoder(w).Encode(err); err != nil {
-			panic(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	}
-	err = model.Save(invoice)
+	invoiceNumber := mux.Vars(r)["invoiceNumber"]
+	invoice.Document = invoiceNumber
+	err = model.Update(&invoice)
 	if (err != nil) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
--- model/invoice.go
+++ model/invoice.go
@@ -33,17 +33,39 @@ func Delete(invoiceNumber string) error {
 	}
 	invoice.IsActive = false
 	invoice.DeactiveAt = time.Now()
-	return Save(invoice)
+	return Save(&invoice)
 }
 
-func Save(i Invoice) error {
-	return database.GetDb().Save(i).Error
+func Update(attributes *Invoice) error {
+	invoice, err := Get(attributes.Document)
+	if (err != nil) {
+		return err
+	}
+	attributes.ID = invoice.ID
+	return Save(attributes)
+}
+
+func Create(attributes *Invoice) error {
+	_, err := Get(attributes.Document)
+	if (attributes.Document == "") {
+		err = errors.New("Attribute Document containing invoice number is required")
+		return err
+	}
+	if (err == nil) {
+		err = errors.New("Invoice [" + attributes.Document + "] already exist")
+		return err
+	}
+	return Save(attributes)
+}
+
+func Save(i *Invoice) error {
+	return database.GetDb().Save(&i).Error
 }
 
 func Get(invoiceNumber string) (Invoice, error) {
 	var invoice Invoice
 	var err error
-	if database.GetDb().Where(&Invoice{Document:invoiceNumber}).Find(&invoice).RecordNotFound() {
+	if database.GetDb().Where("document = ?", invoiceNumber).Find(&invoice).RecordNotFound() {
 		err = errors.New("Invoice [" + invoiceNumber + "] not found")
 		return invoice, err
 	}
