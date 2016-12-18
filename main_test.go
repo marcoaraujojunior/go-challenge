e2657f4b25d12e5c35173d28b53badd7c80eb749
Marco Araujo <marco.araujo.junior@gmail.com>
2016-12-18 21:12:07 -0200

    Test ListInvoices

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main_test
 import(
 	"log"
 	"bytes"
+	"strings"
 	"testing"
 	"net/url"
 	"net/http"
@@ -23,9 +24,14 @@ func beforeTest() {
 
 }
 
-func returnTwoRecords() {
+func setQueryFunc(result string, columns []string) {
 	testdb.SetQueryFunc(func(query string) (driver.Rows, error) {
-		columns := []string{
+		return testdb.RowsFromCSVString(columns, result), nil
+	})
+}
+
+func selectAllColumns() []string {
+	return []string{
 			"id",
 			"reference_month",
 			"reference_year",
@@ -36,57 +42,35 @@ func returnTwoRecords() {
 			"created_at",
 			"deactive_at",
 		}
-		result := `
-		1,10,2016,069,Teste 1,12.32,1,2016-12-13 12:33:42,2016-12-14 18:59:31
-		11,12,2016,6969,Teste 1,12.32,1,2016-12-15 13:24:38,2016-12-14 18:59:31
-		`
-		return testdb.RowsFromCSVString(columns, result), nil
-	})
+}
+
+func returnTwoRecords() {
+	result := `
+	1,10,2016,069,Teste 1,12.32,1,2016-12-13 12:33:42,2016-12-14 18:59:31
+	11,12,2016,6969,Teste 1,12.32,1,2016-12-15 13:24:38,2016-12-14 18:59:31
+	`
+	setQueryFunc(result, selectAllColumns())
+}
+
+func countFiveInvoices() {
+	columns := []string{"count"}
+	result := `5`
+	setQueryFunc(result, columns)
+}
+
+func returnFiveRecords() {
+	result := `
+	1,10,2016,069,Teste 1,12.32,1,2016-12-13 12:33:42,2016-12-14 18:59:31
+	2,11,2016,696,Teste 1,12.32,1,2016-12-15 13:24:38,2016-12-14 18:59:31
+	3,8,2016,6,Teste 1,12.32,1,2016-12-15 13:24:38,2016-12-14 18:59:31
+	10,9,2016,969,Teste 1,12.32,1,2016-12-15 13:24:38,2016-12-14 18:59:31
+	11,12,2016,6969,Teste 1,12.32,1,2016-12-15 13:24:38,2016-12-14 18:59:31
+	`
+	setQueryFunc(result, selectAllColumns())
 }
 
 func returnNoneRecord() {
-	testdb.SetQueryFunc(func(query string) (driver.Rows, error) {
-		columns := []string{
-			"id",
-			"reference_month",
-			"reference_year",
-			"document",
-			"description",
-			"amount",
-			"is_active",
-			"created_at",
-			"deactive_at",
-		}
-		result := `
-		`
-		return testdb.RowsFromCSVString(columns, result), nil
-	})
-}
-
-func TestGetInvoiceShouldReturnStatus404IfInvalidRequest(t *testing.T) {
-	beforeTest()
-	returnTwoRecords()
-	data := url.Values{}
-	req, err := http.NewRequest("GET", "/v1/invoicessss", bytes.NewBufferString(data.Encode()))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req.Header.Set("Authorization", "Bearer dXNlcm5hbWU6cGFzc3dvcmQ")
-
-	rr := httptest.NewRecorder()
-
-	router := route.NewRouter()
-	router.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusNotFound {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
-	}
-
-	expected := "404 page not found\n"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
+	setQueryFunc(``, selectAllColumns())
 }
 
 func TestGetInvoiceShouldReturnStatus404IfInvoiceNotInformed(t *testing.T) {
@@ -164,6 +148,129 @@ func TestGetInvoiceShouldReturnStatus200AndReturnValues(t *testing.T) {
 	expected := `{"ID":11,"ReferenceMonth":12,"ReferenceYear":2016,"Document":"6969","Description":"Teste 1","Amount":12.32,"IsActive":true,"CreatedAt":"0001-01-01T00:00:00Z","DeactiveAt":"0001-01-01T00:00:00Z"}`+ "\n"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestListInvoicesShouldReturnStatus404IfInvalidRequest(t *testing.T) {
+	beforeTest()
+	returnTwoRecords()
+	data := url.Values{}
+	req, err := http.NewRequest("GET", "/v1/invoicessss", bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer dXNlcm5hbWU6cGFzc3dvcmQ")
+
+	rr := httptest.NewRecorder()
+
+	router := route.NewRouter()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+	}
+
+	expected := "404 page not found\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestListInvoicesShouldReturnStatus200AndReturnValues(t *testing.T) {
+	beforeTest()
+	returnTwoRecords()
+	data := url.Values{}
+	req, err := http.NewRequest("GET", "/v1/invoices", bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+	rr := httptest.NewRecorder()
+
+	router := route.NewRouter()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `[{"ID":1,"ReferenceMonth":10,"ReferenceYear":2016,"Document":"069","Description":"Teste 1","Amount":12.32,"IsActive":true,"CreatedAt":"0001-01-01T00:00:00Z","DeactiveAt":"0001-01-01T00:00:00Z"},{"ID":11,"ReferenceMonth":12,"ReferenceYear":2016,"Document":"6969","Description":"Teste 1","Amount":12.32,"IsActive":true,"CreatedAt":"0001-01-01T00:00:00Z","DeactiveAt":"0001-01-01T00:00:00Z"}]`+ "\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestListInvoicesShouldReturnStatus200AndDoNotReturnValues(t *testing.T) {
+	beforeTest()
+	returnNoneRecord()
+	data := url.Values{}
+	req, err := http.NewRequest("GET", "/v1/invoices", bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+	rr := httptest.NewRecorder()
+
+	router := route.NewRouter()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `[]`+"\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestListInvoicesShouldReturnStatus200AndFourLinksHeader(t *testing.T) {
+	beforeTest()
+	returnFiveRecords()
+	countFiveInvoices()
+
+	data := url.Values{}
+	req, err := http.NewRequest("GET", "/v1/invoices?page=3&per_page=1", bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+	rr := httptest.NewRecorder()
+
+	router := route.NewRouter()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	links := strings.SplitN(rr.Header()["Link"][0], ",", 4)
+
+	expectedLinkNext := `</v1/invoices?page=4&per_page=1> ; rel="next"`
+	if links[0] != expectedLinkNext {
+		t.Errorf("handler returned unexpected link header: got %v want %v", links[0], expectedLinkNext)
+	}
+
+	expectedLinkLast := `</v1/invoices?page=5&per_page=1> ; rel="last"`
+	if links[1] != expectedLinkLast {
+		t.Errorf("handler returned unexpected link header: got %v want %v", links[1], expectedLinkLast)
+	}
+
+	expectedLinkFirst := `</v1/invoices?page=1&per_page=1> ; rel="first"`
+	if links[2] != expectedLinkFirst {
+		t.Errorf("handler returned unexpected link header: got %v want %v", links[2], expectedLinkFirst)
+	}
+
+	expectedLinkPrev := `</v1/invoices?page=2&per_page=1> ; rel="prev"`
+	if links[3] != expectedLinkPrev {
+		t.Errorf("handler returned unexpected link header: got %v want %v", links[0], expectedLinkPrev)
 	}
 }
 
