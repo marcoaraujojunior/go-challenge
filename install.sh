62b5295ade539e6a42d9de0ab435ec7759af8e44
Marco Araujo <marco.araujo.junior@gmail.com>
2016-12-20 10:33:20 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 go get github.com/gorilla/mux
 go get github.com/go-sql-driver/mysql
 go get -u github.com/jinzhu/gorm
+go get github.com/erikstmartin/go-testdb
 go get github.com/mattn/goveralls
 go get golang.org/x/tools/cmd/cover
 
